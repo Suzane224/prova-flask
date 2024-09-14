2bfcc28fee9424befd4b14535bbb1424621ce1c1
= <=>
2024-09-13 21:48:26 -0300

    entrega prova 2

--- inicio.py
+++ inicio.py
@@ -1,10 +1,27 @@
-from flask import Flask
+from flask import Flask,render_template,request,redirect,session,url_for
 
 app = Flask(__name__)
+app.secret_key = 'supersecretkey' #substitua por uma chave secreta real
 
 @app.route('/')
 def index():
-    return "Estilo do Rei Barbearia"
+    return render_template('index.html') 
+
+@app.route('/autenticar',methods=['POST','GET'])
+Def autenticar():
+    if request.method =='POST':
+        if request.form['senha'] == 'senac' and request.form['usuario'] =='walter':
+            return 'logado com sucesso'
+        else:
+            return 'erro na autenticacao'
+
+@app.route('/logout')
+def saur():
+    session.pop('usuario_logodo',None)
+    return redirect(url_for('index'))
+
+app.run(debug.true)
+
 
 @app.route('/novofuncionario')
 def funcionario():
