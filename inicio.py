b197e1395cd92a4a1ca132df61e5a3a18c68ae8c
= <=>
2024-09-19 20:03:27 -0300

    Enviando prova IV

--- inicio.py
+++ inicio.py
@@ -1,21 +1,31 @@
-from flask import Flask,render_template,request,redirect,session,url_for
+from flask import Flask,render_template,request,redirect,session,url_for,flash
+from usuario import Usuario
 
 app = Flask(__name__)
 app.secret_key = 'supersecretkey' #substitua por uma chave secreta real
+
+usuario1 = Usuario("Bruno Divino", "BD", "alohomora")
+usuario2 = Usuario("Camila Ferreira", "Mila", "paozinho")
+usuario3 = Usuario("Guilherme Louro", "Cake","python_eh_vida")
+
+usuarios = {usuario1.nome : usuario1,
+             usuario2.nome : usuario2,  
+             usuario3.nome : usuario3 }
+
 
 @app.route('/')
 def index():
     return render_template('index.html') 
 
-@app.route('/autenticar',methods=['POST','GET'])
+@app.route('/autenticar', methods=['POST'])
 def autenticar():
-    if request.method == 'POST':
-        usuario = request.form['usuario']
-        if request.form['senha'] == 'senac' and request.form['usuario'] =='walter':
-            session['usuario_logado'] = request.form['usuario']
-            return 'logado com sucesso'
+    if request.form ['usuario'] in usuarios:
+        usuario = usuarios [request.form['usuario']]
+        if request.form['senha'] == usuario.senha:
+            session['usuario_logado'] = usuario.nome
+            return 'loguei'
         else:
-            return 'erro na autenticacao'
+            return 'erro na autenticacao web'
 
 @app.route('/logout')
 def sair():
@@ -40,11 +50,6 @@ def servico():
 @app.route('/novoagendamento')
 def agendamento():
     return "agendamentoadicionado"
-    
-
-
-
-
 
 app.run(debug=True)
 
